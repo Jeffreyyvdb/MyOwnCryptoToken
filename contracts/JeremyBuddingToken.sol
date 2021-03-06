2707859da363f7d56044a6910bdb315a229ab117
Jeffrey <Jeffrey.2001.2312@gmail.com>
2021-03-06 19:05:08 +0100

    Approval

--- contracts/JeremyBuddingToken.sol
+++ contracts/JeremyBuddingToken.sol
@@ -12,7 +12,17 @@ contract JeremyBuddingToken {
 		uint256 _value
 	);
 
+	// approve
+
+	event Approval(
+		address indexed _owner,
+		address indexed _spender,
+		uint256 _value
+		);
+
 	mapping(address => uint256) public balanceOf;
+	mapping(address => mapping(address => uint256)) public allowance;
+	// allowance
 
 	constructor(uint256 _initialSupply) public {
 		balanceOf[msg.sender] = _initialSupply;
@@ -30,4 +40,15 @@ contract JeremyBuddingToken {
 
 	return true;
 	}
+
+	// approve
+	function approve(address _spender, uint256 _value) public returns (bool success){
+		allowance[msg.sender][_spender] = _value;
+
+		// approve event
+		emit Approval(msg.sender, _spender, _value);
+		return true;
+	}
+	// transferFrom
+
 }
