421deeed0f31dcb7f67afa6cb86b972b70f85f01
Jeffrey <Jeffrey.2001.2312@gmail.com>
2021-03-07 00:52:56 +0100

    Sell / Buy

--- contracts/JeremyBuddingTokenSale.sol
+++ contracts/JeremyBuddingTokenSale.sol
@@ -6,10 +6,27 @@ contract JeremyBuddingTokenSale {
 	address admin;
 	JeremyBuddingToken public tokenContract;
 	uint256 public tokenPrice;
+	uint256 public tokensSold;
+
+	event Sell(address _buyer, uint256 _amount);
 
 	constructor(JeremyBuddingToken _tokenContract, uint256 _tokenPrice) public {
 		admin = msg.sender;
 		tokenContract = _tokenContract;
 		tokenPrice = _tokenPrice;
 	}	
+
+	// multiply
+	function multiply(uint x, uint y) internal pure returns (uint z){
+		require(y == 0 || (z = x * y) / y == x);
+	}
+	function buyTokens(uint256 _numberOfTokens) public payable {
+		require(msg.value == multiply(_numberOfTokens , tokenPrice));
+		require(address(tokenContract).balance >= _numberOfTokens);
+		require(tokenContract.transfer(msg.sender , _numberOfTokens));
+		
+		tokensSold += _numberOfTokens;
+
+		emit Sell(msg.sender,_numberOfTokens);
+	}
 }
