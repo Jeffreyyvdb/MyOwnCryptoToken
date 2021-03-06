351f4374cad050e4a3e8d66bd65a519e19283b04
Jeffrey <Jeffrey.2001.2312@gmail.com>
2021-03-06 20:01:33 +0100

    Delegated transfer

--- contracts/JeremyBuddingToken.sol
+++ contracts/JeremyBuddingToken.sol
@@ -45,10 +45,23 @@ contract JeremyBuddingToken {
 	function approve(address _spender, uint256 _value) public returns (bool success){
 		allowance[msg.sender][_spender] = _value;
 
-		// approve event
 		emit Approval(msg.sender, _spender, _value);
+
 		return true;
 	}
-	// transferFrom
+
+	function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
+		require(_value <= balanceOf[_from]);
+		require(_value <= allowance[_from][msg.sender]);
+
+		balanceOf[_from] -= _value;
+		balanceOf[_to] += _value;
+
+		allowance[_from][msg.sender] -= _value;
+
+		emit Transfer(_from,_to, _value);
+
+		return true;
+	}
 
 }
